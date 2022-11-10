1e0c80a1a4d02fe0301b20f545603c42f86ea11c
Robin <accounts@ganttify.com>
2022-11-10 12:07:26 -0400

    tweaks
    Signed-off-by: Robin <accounts@ganttify.com>

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/flow-invoice/'
-        : '/'
-}
+    publicPath: process.env.NODE_ENV === "production" ? "/flow-invoice/" : "/",
+};
