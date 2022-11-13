02374d58c5b1b0edbb54cb24b1270b7103ad358e
Robin <accounts@ganttify.com>
2022-11-13 15:36:17 -0400

    update with CNAME
    Signed-off-by: Robin <accounts@ganttify.com>

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'flowinvoice.js.org' > CNAME
 
 PACKAGE_VERSION=$(cat ./package.json \
   | grep version \
